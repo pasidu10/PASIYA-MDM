89abe38e5d844a23f8801b9cd43781f6978dfcd4
pasidu10 <punnyanamda.thero@gmail.com>
2025-11-21 20:17:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 🧮 `script.js`
 ```js
 const counterEl = document.getElementById('counter');
-let count = 1700000;        // Start value
+let count = 2000000;        // Start value
 const rate = 5;             // Increment per interval
-const intervalMs = 1000 / rate;  // e.g. 2ms for 5 increments/sec
+const intervalMs = 1000 / rate;  // e.g. 1s for 5 increments/sec
 
 function formatNumber(num) {
   return num.toLocaleString('en-US');
